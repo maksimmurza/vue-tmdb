b6f042dba41ebae3e53d0b67cfe105193224ab53
Maksim Murza <maksim.murza.v@gmail.com>
2021-12-06 16:34:47 +0300

    movie card css refactoring

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,27 +1,24 @@
 <template>
-  <n-card
-    hoverable
-    :style="{
-      position: 'relative',
-    }"
-  >
+  <n-card hoverable class="movie-card">
     <template #cover>
       <router-link :to="'/movie/' + movie.id">
-        <img :src="movieCoverSrc" class="movie-cover" />
+        <img :src="movieCoverSrc" class="movie-card__cover" />
       </router-link>
     </template>
     <div
-      class="rating-label"
+      class="movie-card__rating"
       @mouseover="ratingLabelIsHover = true"
       @mouseleave="ratingLabelIsHover = false"
     >
       <n-icon size="20">
         <star color="gold" size="30px" />
       </n-icon>
-      <span class="rating-text">{{ movie.vote_average }}</span>
-      <span class="rating-text" v-show="ratingLabelIsHover">- {{ movie.vote_count }} votes</span>
+      <span class="movie-card__rating-text">{{ movie.vote_average }}</span>
+      <span class="movie-card__rating-text" v-show="ratingLabelIsHover"
+        >- {{ movie.vote_count }} votes</span
+      >
     </div>
-    <div class="card-text">
+    <div class="movie-card__text">
       <h3 :style="{ marginBottom: '0' }">
         {{ movie.title }}
       </h3>
@@ -40,7 +37,11 @@ export default defineComponent({
     const movieCoverSrc = process.env.VUE_APP_IMG_URL + props.movie.poster_path;
     const ratingLabelIsHover = ref(false);
 
-    return { ...toRefs(props), movieCoverSrc, ratingLabelIsHover };
+    return {
+      ...toRefs(props),
+      movieCoverSrc,
+      ratingLabelIsHover,
+    };
   },
   components: { NCard, NIcon, Star },
   props: {
@@ -50,37 +51,41 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-.rating-label {
-  position: absolute;
-  top: 280px;
-  left: 10px;
-  background-color: white;
-  box-shadow: 0 0 5px 0px black;
-  border-radius: 25px;
-  height: 25px;
-  display: flex;
-  flex-wrap: nowrap;
-  width: fit-content;
-  align-items: center;
-  padding: 3px 10px;
-}
+.movie-card {
+  position: relative;
 
-.rating-text {
-  color: black;
-  font-weight: 700;
-  margin-left: 5px;
-}
+  &__cover {
+    cursor: pointer;
+  }
 
-.card-text {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  min-height: 3em;
-}
+  &__rating {
+    position: absolute;
+    top: 280px;
+    left: 10px;
+    background-color: white;
+    box-shadow: 0 0 5px 0px black;
+    border-radius: 25px;
+    height: 25px;
+    display: flex;
+    flex-wrap: nowrap;
+    width: fit-content;
+    align-items: center;
+    padding: 3px 10px;
+  }
 
-.movie-cover {
-  cursor: pointer;
+  &__rating-text {
+    color: black;
+    font-weight: 700;
+    margin-left: 5px;
+  }
+
+  &__text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    min-height: 3em;
+  }
 }
 </style>
